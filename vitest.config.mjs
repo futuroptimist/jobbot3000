3257dfef0d532664a51dce9c5b91d1a87f640a40
futuroptimist <80668851+futuroptimist@users.noreply.github.com>
2026-06-30 13:57:43 -0700

    Increase Vitest timeout for CI stability

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -3,7 +3,7 @@ import { configDefaults, defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     exclude: [...configDefaults.exclude, 'test/playwright/**'],
-    testTimeout: 15000,
+    testTimeout: 30000,
     pool: 'threads',
     poolOptions: {
       threads: {
